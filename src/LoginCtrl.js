f36fbd59c98008994a4739b42b8b0d44d0b0936a
StuAtGit <stu26code@gmail.com>
2017-04-03 00:14:03 -0700

    Start on login ctrl (mainly testing SSH key)

--- src/LoginCtrl.js
+++ src/LoginCtrl.js
@@ -1,3 +1,17 @@
 //This component will check local storage for a session key
 //verify key, display correct Login/Logout state, and allow
 //one to Login/Logout depending on the current state.
+
+import React, { Component } from 'react';
+
+class LoginCtrl extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+    }
+}
+
+export default LoginCtrl;
