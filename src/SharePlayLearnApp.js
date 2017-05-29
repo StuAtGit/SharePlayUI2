312d06ca90c80e6b7c8a3b25e1058b9fff5db6b4
StuAtGit <stu26code@gmail.com>
2017-05-29 14:49:58 -0700

    Install react-router-dom, rename App

--- src/SharePlayLearnApp.js
+++ src/SharePlayLearnApp.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './SharePlayLearn.css';
 import LoginCtrl from "./LoginCtrl";
 
-class App extends Component {
+class SharePlayLearnApp extends Component {
   render() {
     return (
         <div>
@@ -33,4 +33,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default SharePlayLearnApp;
